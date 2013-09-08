d119a4a64f2a2f33136dc4085b2fb294bd0bd3a3
Jeremy Keeshin <jkeesh@gmail.com>
2013-09-08 12:39:28 -0700

    Fix pep8 style

--- listener.py
+++ listener.py
@@ -1,22 +1,34 @@
-# http://stackoverflow.com/questions/4160175/detect-tap-with-pyaudio-from-live-mic
+"""Listens in the background for input from the mic."""
+
+
+# Audio Code Source
+# http://stackoverflow.com/questions/
+# 4160175/detect-tap-with-pyaudio-from-live-mic
 
 import pyaudio
 import struct
 import math
+import os
+
+TWITTER_USERNAME = '668capp'
+TWITTER_PASSWORD = os.environ['CAPP_PASS']
 
 INITIAL_TAP_THRESHOLD = 0.010
 FORMAT = pyaudio.paInt16
-SHORT_NORMALIZE = (1.0/32768.0)
+SHORT_NORMALIZE = (1.0 / 32768.0)
 CHANNELS = 2
 RATE = 44100
 INPUT_BLOCK_TIME = 0.05
-INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
+INPUT_FRAMES_PER_BLOCK = int(RATE * INPUT_BLOCK_TIME)
 
-OVERSENSITIVE = 15.0/INPUT_BLOCK_TIME
+OVERSENSITIVE = 15.0 / INPUT_BLOCK_TIME
 
-UNDERSENSITIVE = 120.0/INPUT_BLOCK_TIME # if we get this many quiet blocks in a row, decrease the threshold
+# if we get this many quiet blocks in a row, decrease the threshold
+UNDERSENSITIVE = 120.0 / INPUT_BLOCK_TIME
 
-MAX_TAP_BLOCKS = 0.15/INPUT_BLOCK_TIME # if the noise was longer than this many blocks, it's not a 'tap'
+# if the noise was longer than this many blocks, it's not a 'tap'
+MAX_TAP_BLOCKS = 0.15 / INPUT_BLOCK_TIME
+
 
 def get_rms(block):
 
@@ -27,9 +39,9 @@ def get_rms(block):
 
     # we will get one short out for each
     # two chars in the string.
-    count = len(block)/2
-    format = "%dh"%(count)
-    shorts = struct.unpack( format, block )
+    count = len(block) / 2
+    format = "%dh" % (count)
+    shorts = struct.unpack(format, block)
 
     # iterate over the block.
     sum_squares = 0.0
@@ -37,43 +49,47 @@ def get_rms(block):
     # sample is a signed short in +/- 32768.
     # normalize it to 1.0
         n = sample * SHORT_NORMALIZE
-        sum_squares += n*n
+        sum_squares += n * n
 
-    return math.sqrt( sum_squares / count )
+    return math.sqrt(sum_squares / count)
 
-pa = pyaudio.PyAudio()                                 #]
-                                                       #|
-stream = pa.open(format = FORMAT,                      #|
-         channels = CHANNELS,                          #|---- You always use this in pyaudio...
-         rate = RATE,                                  #|
-         input = True,                                 #|
-         frames_per_buffer = INPUT_FRAMES_PER_BLOCK)   #]
+pa = pyaudio.PyAudio()
 
-tap_threshold = INITIAL_TAP_THRESHOLD                  #]
-noisycount = MAX_TAP_BLOCKS+1                          #|---- Variables for noise detector...
-quietcount = 0                                         #|
-errorcount = 0                                         #]
+# You always use this in pyaudio...
+stream = pa.open(format=FORMAT,
+                 channels=CHANNELS,
+                 rate=RATE,
+                 input=True,
+                 frames_per_buffer=INPUT_FRAMES_PER_BLOCK)
+
+tap_threshold = INITIAL_TAP_THRESHOLD
+
+#|---- Variables for noise detector...
+noisycount = MAX_TAP_BLOCKS + 1
+quietcount = 0
+errorcount = 0
 
 for i in range(1000):
-    try:                                                    #]
-        block = stream.read(INPUT_FRAMES_PER_BLOCK)         #|
-    except IOError, e:                                      #|---- just in case there is an error!
-        errorcount += 1                                     #|
-        print( "(%d) Error recording: %s"%(errorcount,e) )  #|
-        noisycount = 1                                      #]
+    try:
+        block = stream.read(INPUT_FRAMES_PER_BLOCK)
+    # just in case there is an error!
+    except IOError, e:
+        errorcount += 1
+        print("(%d) Error recording: %s" % (errorcount, e))
+        noisycount = 1
 
     amplitude = get_rms(block)
-    if amplitude > tap_threshold: # if its to loud...
+    if amplitude > tap_threshold:  # if its to loud...
         quietcount = 0
         noisycount += 1
         if noisycount > OVERSENSITIVE:
-            tap_threshold *= 1.1 # turn down the sensitivity
+            tap_threshold *= 1.1  # turn down the sensitivity
 
-    else: # if its to quiet...
+    else:  # if its to quiet...
 
         if 1 <= noisycount <= MAX_TAP_BLOCKS:
             print 'tap!'
         noisycount = 0
         quietcount += 1
         if quietcount > UNDERSENSITIVE:
-            tap_threshold *= 0.9 # turn up the sensitivity
+            tap_threshold *= 0.9  # turn up the sensitivity
